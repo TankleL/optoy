763cd2a2b55a0f4cd30fefe005e24b9402b4d965
Tian <tankle_@hotmail.com>
2021-12-01 20:26:28 +0800

    set up pages

--- apps/RayDbg/RayDbg/MasterViewer.cpp
+++ apps/RayDbg/RayDbg/MasterViewer.cpp
@@ -1,23 +1,25 @@
 ﻿#include "pch.h"
-#include "MainPage.h"
-#include "MainPage.g.cpp"
+#include "MasterViewer.h"
+#if __has_include("MasterViewer.g.cpp")
+#include "MasterViewer.g.cpp"
+#endif
 
 using namespace winrt;
 using namespace Windows::UI::Xaml;
 
 namespace winrt::raydbg::implementation
 {
-    MainPage::MainPage()
+    MasterViewer::MasterViewer()
     {
         InitializeComponent();
     }
 
-    int32_t MainPage::MyProperty()
+    int32_t MasterViewer::MyProperty()
     {
         throw hresult_not_implemented();
     }
 
-    void MainPage::MyProperty(int32_t /* value */)
+    void MasterViewer::MyProperty(int32_t /* value */)
     {
         throw hresult_not_implemented();
     }
